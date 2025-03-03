a44558f429e6e4952bfbc5e7a2a84eb2e2f3e23b
unknown <flavio.santos.ti@outlook.com>
2025-03-02 21:19:33 -0300

    ♻️ Refactor: improved code structure and readability.

--- Backend/src/BaseHours.Application/Interfaces/IClientService.cs
+++ Backend/src/BaseHours.Application/Interfaces/IClientService.cs
@@ -1,13 +1,14 @@
 ﻿using BaseHours.Application.Dtos;
+using FDS.NetCore.ApiResponse.Models;
 
 namespace BaseHours.Application.Interfaces;
 
 public interface IClientService
 {
+    Task<Response<ClientDto>> AddAsync(ClientRequestDto request);
+    Task<Response<bool>> DeleteAsync(Guid id);
     Task<IEnumerable<ClientDto>> GetAllAsync();
     Task<ClientDto?> GetByIdAsync(Guid id);
     Task<IEnumerable<ClientDto>> SearchByNameAsync(string name);
-    Task AddAsync(ClientDto clientDto);
     Task UpdateAsync(ClientDto clientDto);
-    Task DeleteAsync(Guid id);
 }
