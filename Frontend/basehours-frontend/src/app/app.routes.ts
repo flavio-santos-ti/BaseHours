2d77092b679dafe92855658ebabc5b0024e42905
unknown <flavio.santos.ti@outlook.com>
2025-04-11 00:46:09 -0300

    🔧 Fix(sidebar): correct active links and rename to BaseHours.

--- Frontend/basehours-frontend/src/app/app.routes.ts
+++ Frontend/basehours-frontend/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { ClientEditPage } from './pages/client-edit/client-edit.page';
 import { HomePageComponent } from './pages/home/home.page'; // adicionar esse import
 
 export const routes: Routes = [
-  { path: '', component: HomePageComponent }, // agora a tela inicial está limpa
+  { path: '', component: HomePageComponent }, // tela inicial limpa
   { path: 'clients', component: ClientListPage },
   { path: 'clients/create', component: ClientCreatePage },
   { path: 'clients/edit/:id', component: ClientEditPage },
